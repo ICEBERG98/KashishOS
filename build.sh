cd8666f6c181ea03449aeb08936b4ac8cea1a2eb
Kashish Soni <kashish.soni1998@gmail.com>
2019-01-20 09:07:23 +0000

    Added Build Script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+#export PATH="$HOME/opt/cross/bin:$PATH"
+$HOME/opt/cross/bin/i686-elf-gcc -c ./src/kernel.c -o ./target/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+$HOME/opt/cross/bin/i686-elf-gcc -c ./src/gdt.c -o ./target/gdt.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra               
+$HOME/opt/cross/bin/i686-elf-as ./src/boot.s -o ./target/boot.o
+$HOME/opt/cross/bin/i686-elf-gcc -T ./src/linker.ld -o ./isodir/boot/myos.bin -ffreestanding -O2 -nostdlib ./target/boot.o ./target/gdt.o ./target/kernel.o -lgcc
+./toDisk.sh
+                                                                    
--- toDisk.sh
+++ toDisk.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-if grub-file --is-x86-multiboot myos.bin; then   echo 'multiboot confirmed\n'; else   echo 'the file is not multiboot\n'; fi
+if grub-file --is-x86-multiboot ./isodir/boot/myos.bin; then   echo 'multiboot confirmed'; else   echo 'the file is not multiboot'; fi
+echo '----------------------buiding iso-------------------'
 #cp myos.bin isodir/boot/myos.bin
 #cp grub.cfg isodir/boot/grub/grub.cfg
 grub-mkrescue -o myos.iso isodir
-echo 'ISO Created\n'
+echo '-----------------------ISO Created------------------'
