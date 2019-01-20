ed555f9b00495be27337e5b6c3f19b6d7642e817
Kashish Soni <kashish.soni1998@gmail.com>
2019-01-20 09:08:56 +0000

    Added Build Script

--- build.sh
+++ build.sh
@@ -5,5 +5,4 @@ $HOME/opt/cross/bin/i686-elf-gcc -c ./src/kernel.c -o ./target/kernel.o -std=gnu
 $HOME/opt/cross/bin/i686-elf-gcc -c ./src/gdt.c -o ./target/gdt.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra               
 $HOME/opt/cross/bin/i686-elf-as ./src/boot.s -o ./target/boot.o
 $HOME/opt/cross/bin/i686-elf-gcc -T ./src/linker.ld -o ./isodir/boot/myos.bin -ffreestanding -O2 -nostdlib ./target/boot.o ./target/gdt.o ./target/kernel.o -lgcc
-./toDisk.sh
-                                                                    
+./scripts/toDisk.sh                                                                    
